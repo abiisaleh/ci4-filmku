914c7a5a5e022cfc4ace2d3dee8aea16dd5c3741
Muhamad abi saleh <abi.saleh.25@gmail.com>
2022-09-20 18:02:50 +0900

    torrent detail

--- app/Views/detail.php
+++ app/Views/detail.php
@@ -178,5 +178,41 @@
                 console.log('torrent download finished')
             })
         })
+
+        function onTorrent(torrent) {
+            log('Got torrent metadata!')
+            log(
+                'Torrent info hash: ' + torrent.infoHash + ' ' +
+                '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
+                '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
+            )
+
+            // Print out progress every 5 seconds
+            const interval = setInterval(function() {
+                log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
+            }, 5000)
+
+            torrent.on('done', function() {
+                log('Progress: 100%')
+                clearInterval(interval)
+            })
+
+            // Render all files into to the page
+            torrent.files.forEach(function(file) {
+                file.appendTo('.log')
+                log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
+                file.getBlobURL(function(err, url) {
+                    if (err) return log(err.message)
+                    log('File done.')
+                    log('<a href="' + url + '">Download full file: ' + file.name + '</a>')
+                })
+            })
+        }
+
+        function log(str) {
+            const p = document.createElement('p')
+            p.innerHTML = str
+            document.querySelector('.log').appendChild(p)
+        }
     </script>
     <?php $this->endsection() ?>
